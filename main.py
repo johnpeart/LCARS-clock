93a8a3dbaa615ecc78555b7aea5634555e84de30
John Peart <code@johnpe.art>
2021-09-11 22:18:35 +0100

    Add comments

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import chooseDisplay as choose
 import showDisplay as display
 
-displayType = choose.displayType() # set to 'clock' or 'wordclock'
 
 def main():
+    # determines what to show
+    displayType = choose.displayType() 
+    # sets up what the display will show
     display.updateDisplay(displayType)
+    # updates the GUI
     display.mainloop()
 
 if __name__ == "__main__":

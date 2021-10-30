9327b1d4867cd912e7c3fa296799957bdeb297c1
John Peart <code@johnpe.art>
2021-10-30 16:11:42 +0100

    Update for alerts

--- alerts.py
+++ alerts.py
@@ -6,12 +6,20 @@ def checkMessages():
     # This function should check the settings > message file for any significant birthdays.
     # If a birthday is found, it should return the person's name, birthday, and age.
 
-    first_date = "30/11/2020"
-    second_date = "12/10/2019"
 
-    formatted_date1 = time.strptime(first_date, "%d/%m/%Y")
-    formatted_date2 = time.strptime(second_date, "%d/%m/%Y")
-    print(formatted_date1 > formatted_date2)
+    # Get all the dates
+
+
+    # For each date, check if it's less than 7 days from now
+
+        # now = "30/11/2020"
+        # second_date = "12/10/2019"
+
+        # formatted_date1 = time.strptime(first_date, "%d/%m/%Y")
+        # formatted_date2 = time.strptime(second_date, "%d/%m/%Y")
+        # print(formatted_date1 > formatted_date2)
+
+    
 
     alertType = 'red'
     messageText = 'This is a yellow alert message'
